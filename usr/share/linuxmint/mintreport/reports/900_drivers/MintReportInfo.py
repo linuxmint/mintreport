5e2a966fb56f4bd0cb5a492110772fd9d2c95aa1
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2019-12-05 15:59:08 +0000

    Drivers: Ignore VMware and Virtualbox recommendations
    We recommend upstream drivers for these

--- usr/share/linuxmint/mintreport/reports/900_drivers/MintReportInfo.py
+++ usr/share/linuxmint/mintreport/reports/900_drivers/MintReportInfo.py
@@ -24,15 +24,17 @@ class Report(InfoReport):
         try:
             from UbuntuDrivers import detect
             devices = detect.system_device_drivers()
-            for device_name in devices:
-                device = devices[device_name]
+            for device_id in devices:
+                device = devices[device_id]
+                device_name = "%s %s" % (device['vendor'], device['model'])
+                if "virtualbox" in device_name.lower() or "vmware" in device_name.lower():
+                    print("Ignoring %s" % device_name)
+                    # Ignore them, they're not recommended
+                    continue
                 packages = []
                 for driver_name in device['drivers']:
                     driver = device['drivers'][driver_name]
                     if 'builtin' in driver.keys() and driver['builtin'] == True:
-                        continue
-                    if "virtualbox" in driver_name:
-                        # Ignore them, they're not recommended
                         continue
                     packages.append(driver_name)
                 any_installed = False
@@ -42,7 +44,7 @@ class Report(InfoReport):
                         any_installed = True
                         break
                 if not any_installed:
-                    self.drivers.append("%s %s" % (device['vendor'], device['model']))
+                    self.drivers.append(device_name)
             return (len(self.drivers) > 0)
         except Exception as e:
             print ("Failed to assess drivers availability: %s" % e)
