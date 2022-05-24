42e41c5f4740e58bb6695872809f29c63991a9cb
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2022-05-24 17:19:47 +0100

    tray: Add a process monitor
    This shows a tray icon whenever a known lengthy operation is detected such as:
    
    - Mintupdate automatic updates
    - Timeshift automatic system snapshots

--- usr/lib/linuxmint/mintreport/tray.py
+++ usr/lib/linuxmint/mintreport/tray.py
@@ -3,6 +3,7 @@ import gettext
 import gi
 import locale
 import os
+import psutil
 import setproctitle
 import subprocess
 import xapp.os
@@ -34,7 +35,11 @@ class GtkStatusIcon(Gtk.StatusIcon):
     def set_icon_name(self, icon_name):
         self.set_from_icon_name(icon_name)
 
-
+class MonitoredProcess():
+    def __init__(self, name, cmdline, description):
+        self.name = name
+        self.cmdline = cmdline
+        self.description = description
 
 class MyApplication(Gtk.Application):
     # Main initialization routine
@@ -67,6 +72,13 @@ class MyApplication(Gtk.Application):
             self.status_icon.connect("popup-menu", self.on_gtk_statusicon_popup)
         self.status_icon.set_visible(False)
 
+        # Monitor icon
+        self.monitor_icon = XApp.StatusIcon()
+        self.monitor_icon.set_name("process-monitor")
+        self.monitor_icon.set_visible(False)
+        self.monitoring_process = None
+        self.monitor_source_id = None
+
     def on_statusicon_activated(self, icon, button, time):
         if button == Gdk.BUTTON_PRIMARY:
             GLib.spawn_async(["/usr/bin/mintreport"])
@@ -92,6 +104,9 @@ class MyApplication(Gtk.Application):
             # Auto-refresh
             if self.settings.get_boolean("autorefresh"):
                 GLib.timeout_add_seconds(self.settings.get_int("autorefresh-seconds"), self.load_info)
+            # Auto-monitor
+            if self.settings.get_boolean("automonitor"):
+                GLib.timeout_add_seconds(self.settings.get_int("automonitor-seconds"), self.monitor)
         else:
             print("Already running!")
 
@@ -118,6 +133,42 @@ class MyApplication(Gtk.Application):
         else:
             self.status_icon.set_visible(False)
 
+        return True
+
+    def is_process_running(self, process):
+        for p in psutil.process_iter():
+            if process.cmdline in ' '.join(p.cmdline()):
+                return True
+        return False
+
+    def monitor(self):
+        if self.monitoring_process != None:
+            return
+        processes = []
+        processes.append(MonitoredProcess(_("System Snapshots"), "timeshift --", _("Timeshift is running.")))
+        processes.append(MonitoredProcess(_("Automatic Updates"), "/usr/lib/linuxmint/mintUpdate/automatic_upgrades.py", _("Automatic updates are being installed.")))
+        self.monitor_icon.set_visible(False)
+        for process in processes:
+            if self.is_process_running(process):
+                # A process is found running!
+                # Show a tray icon
+                self.monitoring_process = process
+                self.monitor_icon.set_visible(True)
+                self.monitor_icon.set_icon_name("system-run-symbolic")
+                tooltip = "<b>%s</b>\n%s" % (process.name, process.description)
+                self.monitor_icon.set_tooltip_text(tooltip)
+                # Start a clean up thread to hide the tray when it finishes
+                self.monitor_source_id = GLib.timeout_add_seconds(2, self.clean_up)
+        return True
+
+    def clean_up(self):
+        if self.monitoring_process == None:
+            return
+        if not self.is_process_running(self.monitoring_process):
+            # The process is finished, hide the tray
+            self.monitoring_process = None
+            self.monitor_icon.set_visible(False)
+            GLib.source_remove(self.monitor_source_id)
         return True
 
 if __name__ == "__main__":
