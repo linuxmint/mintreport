f5cd0c6aa8467c1f164fa052792701a7fe4c72e3
Domenico <50798132+Domefemia@users.noreply.github.com>
2022-08-12 22:38:17 +0200

    Made the code more clean

--- usr/share/linuxmint/mintreport/reports/900_drivers/MintReportInfo.py
+++ usr/share/linuxmint/mintreport/reports/900_drivers/MintReportInfo.py
@@ -30,22 +30,18 @@ class Report(InfoReport):
     def construct_name(self, device):
         model_name = device.get('model', None)
         vendor_name = device.get('vendor', None)
-        device_name = None
 
-        is_cpu = False
-        if "intel-microcode" in device['drivers'] or "amd64-microcode" in device['drivers']:
-            is_cpu = True
+        is_cpu = "intel-microcode" in device['drivers'] or "amd64-microcode" in device['drivers']
         if is_cpu:
-            device_name = self.get_cpu_name()
+            return self.get_cpu_name()
         elif vendor_name is None and model_name is None:
-            device_name = _("Unknown")
+            return _("Unknown")
         elif vendor_name is None:
-            device_name = model_name
+            return model_name
         elif model_name is None:
-            device_name = _("%s (unknown)" % vendor_name)
+            return _(f"{vendor_name} (unknown)")
         else:
-            device_name = "%s: %s" % (vendor_name, model_name)
-        return device_name
+            return = f"{vendor_name}: {model_name}"
 
     def is_pertinent(self):
         # Defines whether this report should show up
@@ -68,46 +64,36 @@ class Report(InfoReport):
                 device = devices[device_id]
                 device_name = self.construct_name(device)
                 if "virtualbox" in device_name.lower() or "vmware" in device_name.lower():
-                    print("Ignoring %s" % device_name)
+                    print(f"Ignoring {device_name}")
                     # Ignore them, they're not recommended
                     continue
                 if 'manual_install' in device.keys() and device['manual_install'] == True:
-                    print("Ignoring device: %s (manual_install)" % device_name)
+                    print(f"Ignoring device: {device_name} (manual_install)")
                     continue
                 packages = []
                 for driver_name in device['drivers']:
                     driver = device['drivers'][driver_name]
                     if 'builtin' in driver.keys() and driver['builtin'] == True:
-                        print("Ignoring driver: %s (builtin)" % driver)
+                        print(f"Ignoring driver: {driver} (builtin)")
                         continue
                     packages.append(driver_name)
-                any_installed = False
-                for pkg in packages:
-                    # Check if the package is installed
-                    if os.path.exists("/var/lib/dpkg/info/%s.list" % pkg):
-                        any_installed = True
-                        break
+                any_installed = any(os.path.exists(f"/var/lib/dpkg/info/{pkg}.list") for pkg in packages)
                 if not any_installed:
                     self.drivers.append(device_name)
             return (len(self.drivers) > 0)
         except Exception as e:
-            print ("Failed to assess drivers availability: %s" % e)
+            print(f"Failed to assess drivers availability: {e}")
             return False
 
     def get_descriptions(self):
         # Return the descriptions
-        descriptions = []
-        descriptions.append(_("Hardware drivers are available for the following devices:"))
-        descriptions.append("\n".join(self.drivers))
-        return descriptions
+        return [_("Hardware drivers are available for the following devices:"), "\n".join(self.drivers)]
 
     def get_actions(self):
         # Return available actions
-        actions = []
         action = InfoReportAction(label=_("Launch the Driver Manager"), callback=self.callback)
         action.set_style(Gtk.STYLE_CLASS_SUGGESTED_ACTION)
-        actions.append(action)
-        return actions
+        return [action]
 
     def callback(self, data):
         subprocess.run(["pkexec", "driver-manager"])
