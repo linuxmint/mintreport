b49e3c3386cc65adc5f4546ada2c0d6c7d62ee31
Domenico <50798132+Domefemia@users.noreply.github.com>
2022-08-12 22:20:31 +0200

    Made the code more clean

--- usr/share/linuxmint/mintreport/reports/200_xappstatusapplet-blocked/MintReportInfo.py
+++ usr/share/linuxmint/mintreport/reports/200_xappstatusapplet-blocked/MintReportInfo.py
@@ -19,34 +19,27 @@ class Report(InfoReport):
         self.has_ignore_button = True
         self.indicator_path = None
         self.indicator_pkgname = None
-        self.indicators = []
-        self.indicators.append(["indicator-application", "/usr/lib/systemd/user/indicator-application.service"])
-        self.indicators.append(["ayatana-indicator-application", "/usr/lib/systemd/user/ayatana-indicator-application.service"])
+        self.indicators = [["indicator-application", "/usr/lib/systemd/user/indicator-application.service"], 
+                            ["ayatana-indicator-application", "/usr/lib/systemd/user/ayatana-indicator-application.service"]]
         self.packages = []
 
     def is_pertinent(self):
         # Defines whether this report should show up
-        for name, path in self.indicators:
-            if os.path.exists(path):
-                self.packages.append(name)
+        self.packages.extend([name for name, path in self.indicators if os.path.exists(path)])
         return (len(self.packages) > 0)
 
     def get_descriptions(self):
         # Return the descriptions
-        descriptions = []
-        descriptions.append(_("The following packages prevent tray icons from working properly:"))
+        descriptions = [_("The following packages prevent tray icons from working properly:")]
         descriptions.append("\n".join(self.packages))
         descriptions.append(_("Note: You will need to log out after removing them."))
         return descriptions
 
     def get_actions(self):
         # Return available actions
-        actions = []
-        action = InfoReportAction(label=_("Remove the conflicting packages."),
-                                  callback=self.callback)
+        action = InfoReportAction(label=_("Remove the conflicting packages."),callback=self.callback)
         action.set_style(Gtk.STYLE_CLASS_SUGGESTED_ACTION)
-        actions.append(action)
-        return actions
+        return [action]
 
     def callback(self, data):
         self.remove_packages(self.packages)
