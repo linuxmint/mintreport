558e46932ff9d4ac5da0673b08ac5b4fc1931245
Michael Webster <miketwebster@gmail.com>
2023-08-11 11:09:59 -0400

    Fix error handling of inxi output.
    Fixes #72 (thanks @dead-flunky).
    Fixes #92.

--- usr/lib/linuxmint/mintreport/app.py
+++ usr/lib/linuxmint/mintreport/app.py
@@ -271,7 +271,7 @@ class MintReportWindow():
     @_async
     def load_sysinfo(self):
         try:
-            sysinfo = subprocess.check_output("LANG=C inxi -Fxxrzc0 --usb", shell=True).decode("UTF-8")
+            sysinfo = subprocess.check_output("LANG=C inxi -Fxxrzc0 --usb", shell=True).decode("utf-8", errors='replace')
             self.add_sysinfo_to_textview(sysinfo)
             with open(TMP_INXI_FILE, "w") as f:
                 f.write(sysinfo)
